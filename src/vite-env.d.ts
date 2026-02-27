8f1f13ddd6a1053b0e3738399fef3f464581e9b0
geobusinessconsultancy-glitch <geobusinessconsultancy-glitch>
2026-02-27 14:19:49 +0530

    ..

---
+++ src/vite-env.d.ts
@@ -0,0 +1,26 @@
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const content: string;
+  export { ReactComponent };
+  export default content;
+}
